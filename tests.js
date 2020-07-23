4230ca081f0d5f34a3be95eac240c6beaaab638f
Sebastian <sebasgrammar@hotmail.com>
2020-07-23 00:15:23 -0500

    trying to get it to work the way it should

--- tests.js
+++ tests.js
@@ -1,5 +1,7 @@
 let bars = document.querySelectorAll(".bar")
 
+let steps = document.querySelectorAll(".step")
+
 let kick = document.querySelector("audio")
 
 for (let bar of bars) {
@@ -22,16 +24,81 @@ document.addEventListener("keypress", function (event) {
     }
 })
 
+// for (let index = 0; index < bars.length; index++) {
+//     setTimeout(function (y) {
+//         console.log("XD")
+//         console.log(y)
+//         kick.play()
+//     }, index * 1000, "lol")
+// }
 
-for (let index = 0; index < bars.length; index++) {
-    setTimeout(function (y) {
-        console.log("XD")
-        console.log(y)
-        kick.play()
-    }, index * 1000, "lol")
-}
+/*****/
+
+// for (let index = 0; index < bars.length * 4; index++) {
+//     setTimeout(function (y) {
+//         console.log("XD")
+//         console.log(y)
+//         //kick.play()
+//     }, index * 500, "lol") // gotta implement different bpms
+// }
 
 
 
+
+// for (let index = 0; index < bars.length * 4; index++) {
+//     setTimeout(function (y) {
+//         if (steps[index].classList.contains("active")) {
+//             kick.play()
+//         }
+//         //kick.play()
+//     }, index * 500, "lol") // gotta implement different bpms
+// }
+
+
+// setInterval(function() {
+//     for (let index = 0; index < bars.length * 4; index++) {
+//         setTimeout(function (y) {
+//             if (steps[index].classList.contains("active")) {
+//                 console.log(index)
+//                 kick.play()
+//             }
+//             //kick.play()
+//         }, index * 500, "lol") // gotta implement different bpms
+//     }
+//     console.log("Buuuuu")
+// }, 10000)
+
+// let time = 1000
+
+// setInterval(function() {
+//     for (let index = 0; index < bars.length * 4; index++) {
+//         setTimeout(function (y) {
+//             if (steps[index].classList.contains("active")) {
+//                 console.log(index)
+//                 kick.play()
+//             }
+//             //kick.play()
+//         console.log(index)
+//         }, index * (time / 16), "lol") // gotta implement different bpms
+//     }
+//     console.log("Buuuuu")
+// }, time)
+
+let time = 3000
+
+setInterval(function() {
+    for (let index = 0; index < bars.length * 4; index++) {
+        setTimeout(function (y) {
+            if (steps[index].classList.contains("active")) {
+                console.log(index)
+                console.log("PLAY")
+                kick.play()
+            }
+            //kick.play()
+        console.log(index)
+        }, index * (time / 16), "lol") // gotta implement different bpms
+    }
+    console.log("Buuuuu")
+}, time)
 
 console.log(bars)
